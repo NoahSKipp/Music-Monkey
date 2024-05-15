535c6103cf558b766be761d3f4a92e069a411130
Noah S. Kipp <159732895+NoahSKipp@users.noreply.github.com>
2024-05-15 16:17:30 +0200

    Update config.py
    Author's notes added

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+# ========================================= #
+# Author: Noah S. Kipp                      #
+# Collaborator: Samuel Jaden Garcia Munoz   #
+# Created on: 21.04.2024                    #
+# ========================================= #
+
 TOKEN = 'MTIyODA3MTE3NzIzOTUzMTYyMA.GQUCqP.qEPmDxcJ7RemnBtd-KSFbs7ZdemcNqk9mdvWog'
 APPLICATION_ID = '1228071177239531620'
 GEMINI = "AIzaSyDOkobbmuDYQSuomxs4syj3aEcaghP5eNs"
