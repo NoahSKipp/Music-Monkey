7445b16f6307a21b4f1f5dbc4fed54247a4fed47
NoahSKipp <159732895+NoahSKipp@users.noreply.github.com>
2024-07-15 20:47:27 +0200

    Implemented a /monkey command to send a random monkey image

---
+++ monkeyimages.py
@@ -0,0 +1,34 @@
+# ========================================= #
+# Author: Noah S. Kipp                      #
+# Collaborator: Samuel Jaden Garcia Munoz   #
+# Created on: 13.07.2024                    #
+# ========================================= #
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+import random
+
+class MonkeyImages(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="monkey", description="Send a random picture of a primate")
+    async def monkey(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+
+        width = 500
+        height = 350
+        random_value = random.randint(1, 1000)  # Generate a random value between 1 and 1000
+
+        url = f"https://placemonkeys.com/{width}/{height}?random={random_value}"
+
+        embed = discord.Embed(
+            title="Here's a random monkey picture for you! 🐵",
+            color=discord.Color.green()
+        )
+        embed.set_image(url=url)
+        await interaction.followup.send(embed=embed, ephemeral=False)
+
+async def setup(bot):
+    await bot.add_cog(MonkeyImages(bot))
